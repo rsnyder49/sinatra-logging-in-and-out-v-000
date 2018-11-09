b9547d6ac0fe5fbc21cd3354c82867fb273d7bb2
Robert Snyder <r.snyder49@gmail.com>
2018-11-09 01:37:03 +0000

    automatically backed up by learn

--- db/migrate/20181109013616_create_users.rb
+++ db/migrate/20181109013616_create_users.rb
@@ -1,6 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string 
+      t.string :username
+      t.string :password 
+      t.string :
   end
 end
