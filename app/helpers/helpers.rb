6da086c0f912cf5a5faad90884ebf22e88adde7c
Robert Snyder <r.snyder49@gmail.com>
2018-11-09 01:29:44 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
 class Helpers
+  def self.current_user(session)
+    @user = User.find_by(session[:user_id])
+  end 
+  
+  def self.is_logged_in?(session)
+    if !!self.current_user(session)
+      true 
+    else 
+      false
+  end 
 end
